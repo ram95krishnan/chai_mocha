6bc89525de3a350033fe5d94a2f452275fa61473
Ramakrishnan . S <Ramakrishnan.S@nationstarmail.com>
2016-10-11 23:00:57 +0530

    API TEST

--- test/test.js
+++ test/test.js
@@ -1,7 +1,20 @@
-var expect = require('chai').expect;
+var chai = require('chai');
+var expect = chai.expect;
+var request = require("request");
+var baseUrl = "http://polls.apiblueprint.org";
+var util= require("util");
 
-describe('A basic test', function (){
-  it('should pass when all is well', function() {
-    expect(true).to.be.true;
-  })
+// refer http://docs.feedback13.apiary.io/#reference/0/questions-collection/list-all-questions
+
+describe("API Test",function(){
+  it("Passes for 200",function(done) {
+    request.get({url: baseUrl + "/questions/2926"},
+        function(error,response,body) {
+        //  var bodyObj = JSON.parse(body);
+        //  expect(bodyObj.question).to.equal("Favourite programming language?");
+          expect(response.statusCode).to.equal(200);
+          console.log(body);
+          done();
+        });
+  });
 });
